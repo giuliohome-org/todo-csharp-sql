614bfff2e3b697221b931295d0a305fb71553d0f
Giulio <3272563+giuliohome@users.noreply.github.com>
2023-10-09 00:58:51 +0200

    post action to retrieve the lists

--- src/api/ListsController.cs
+++ src/api/ListsController.cs
@@ -14,7 +14,7 @@ public class ListsController : ControllerBase
         _repository = repository;
     }
 
-    [HttpGet("protected")]
+    [HttpPost("protected")]
     [Authorize("read:lists")]
     [ProducesResponseType(200)]
     public async Task<ActionResult<IEnumerable<TodoList>>> GetLists([FromQuery] int? skip = null, [FromQuery] int? batchSize = null)
