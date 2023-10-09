4b86475906ee891addcede7038b4ad2cf69c5923
Giulio <3272563+giuliohome@users.noreply.github.com>
2023-10-09 10:05:44 +0200

    login redirect

--- src/web/src/layout/header.tsx
+++ src/web/src/layout/header.tsx
@@ -64,10 +64,7 @@ const Header: FC = (): ReactElement => {
                     {isLoading && (<div>Loading...</div>) }
 
                     {!isLoading && !isAuthenticated && 
-                        (<button onClick={() => 
-                            logout({ 
-                                logoutParams: { returnTo: window.location.origin } })}> 
-                                Log Out </button>)}
+                        (<button onClick={() => loginWithRedirect()}>Log In</button>)}
 
                     {/* <Persona size={PersonaSize.size24} text="Sample User" /> */}
                     {/* <Toggle label="Dark Mode" inlineLabel styles={{ root: { marginBottom: 0 } }} onChange={changeTheme} /> */}
