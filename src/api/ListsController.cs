0df94b9597991f52ef4c46329f92b84cab9e082d
Giulio <3272563+giuliohome@users.noreply.github.com>
2023-10-09 23:59:13 +0200

    read:lists

--- src/api/ListsController.cs
+++ src/api/ListsController.cs
@@ -16,7 +16,7 @@ public class ListsController : ControllerBase
 
     // [HttpGet("public")]
     
-    [HttpGet("private-scoped")]
+    [HttpGet]
     [Authorize("read:lists")]
     [ProducesResponseType(200)]
     public async Task<ActionResult<IEnumerable<TodoList>>> GetLists([FromQuery] int? skip = null, [FromQuery] int? batchSize = null)
