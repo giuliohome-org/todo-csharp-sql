e70905c4cbd5c3a078edfd37fb2966b7258eb6f8
Giulio <3272563+giuliohome@users.noreply.github.com>
2023-10-09 09:31:54 +0200

    no unused vars

--- src/web/src/layout/header.tsx
+++ src/web/src/layout/header.tsx
@@ -1,4 +1,4 @@
-import { FontIcon, getTheme, IconButton, IIconProps, IStackStyles, mergeStyles, Persona, PersonaSize, Stack, Text } from '@fluentui/react';
+import { FontIcon, getTheme, IconButton, IIconProps, IStackStyles, mergeStyles, Stack, Text } from '@fluentui/react';
 import React, { FC, ReactElement } from 'react';
 import { useAuth0 } from "@auth0/auth0-react";
 
